2fe2240726b638aa4b33cd26a6ecc91e0725b7bc
Nathan Krueger <natekrueger805@gmail.com>
2026-01-23 21:36:11 -0700

    Fix sensor init

--- sensors.py
+++ sensors.py
@@ -1,13 +1,14 @@
 from smbus2 import SMBus
 from bme280 import BME280
+from time import sleep
 
 def init_bme280(smbus: int = 1) -> BME280:
     # Initialise the BME280
     bus = SMBus(smbus)
     bme = BME280(i2c_dev=bus)
 
-    # Flush the first few junk readings
+    # Flush the first junk reading
     bme.get_temperature()
-    bme.get_temperature()
+    sleep(1.0)
 
     return bme
