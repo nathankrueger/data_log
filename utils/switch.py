52d4e5e3b328cd077b9a6dc238acb7f2b88b79fb
Nathan Krueger <natekrueger805@gmail.com>
2026-01-31 12:49:14 -0700

    Fix unittests

--- utils/switch.py
+++ utils/switch.py
@@ -1,6 +1,7 @@
 from gpiozero import Button
 from signal import pause
 from time import sleep
+import threading
 
 # Define the microswitch connected to GPIO 16
 # Button defaults to using an internal pull-up resistor (3.3V)
@@ -8,10 +9,12 @@ from time import sleep
 microswitch = Button(16)
 
 def switch_pressed():
-    print("Microswitch Activated! (Door Closed/Limit Reached)")
+    thread = threading.current_thread()
+    print(f"--- Switch Pressed! --- [thread_id: {thread.ident}]")
 
 def switch_released():
-    print("Microswitch Released!")
+    thread = threading.current_thread()
+    print(f"--- Switch Released! --- [thread_id: {thread.ident}]")
 
 # Set up the event handlers
 microswitch.when_pressed = switch_pressed
@@ -20,5 +23,6 @@ microswitch.when_released = switch_released
 print("Waiting for microswitch input... Press Ctrl+C to exit")
 
 while True:
-    print("Main Loop...")
+    thread = threading.current_thread()
+    print(f"Main Loop...  [thread_id: {thread.ident}]")
     sleep(1)
